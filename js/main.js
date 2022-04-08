e8281bbee42dc8d34a8508791872f8a49abecde0
ChanelT <crene7722@gmail.com>
2022-04-08 09:14:51 -0500

    added carousel toggle

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@ function getDrink() {
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data.drinks[0]);
+            if(document.querySelector('.carousel').classList.contains('hidden')){
+                document.querySelector('.carousel').classList.toggle('hidden')
+            }
+            
             document.querySelector("h2").innerText = data.drinks[0].strDrink;
             document.querySelector("img").src = data.drinks[0].strDrinkThumb;
             document.querySelector("h3").innerText =
